d1d3ae60164d8cf3922d3fae77b9827d4bfcf332
Francisco Moral <francisco@rain.us>
2022-09-15 00:45:17 -0300

    Refactor: negative test

--- cypress/integration/context/registration.spec.js
+++ cypress/integration/context/registration.spec.js
@@ -11,6 +11,12 @@ describe('Registration', () => {
     r.validateRegisterPet();
   });
 
+  it('should not register a new pet due to wrong input', () => {
+    r.invalidRegisterPet();
+    cy.contains('Pet name has already been taken');
+    cy.contains('Pet description is too short (minimum is 10 characters)');
+  });
+
   it('should delete my pet', () => {
     r.deletePet();
   });
--- cypress/support/commons/elements.js
+++ cypress/support/commons/elements.js
@@ -28,8 +28,8 @@ class RegisterPet {
       this.CommonElements.registerBtn().click();
       this.CommonElements.newPetBtn().click();
       this.CommonElements.registerMessage().should('contain', 'New Pet registration');
-      this.CommonElements.petOwner().type(data.users.name[0]+Math.floor(Math.random() * 100));
-      this.CommonElements.petName().type(data.pets.name[0]+Math.floor(Math.random() * 100));
+      this.CommonElements.petOwner().type(data.users.name[0]+Math.floor(Math.random() * 1000));
+      this.CommonElements.petName().type(data.pets.name[0]+Math.floor(Math.random() * 1000));
       this.CommonElements.petColor().select('brown');
       this.CommonElements.petAge().type('2');
       this.CommonElements.petDescription().type(
@@ -49,6 +49,26 @@ class RegisterPet {
     this.CommonElements.table().should('contain', 'Fido'); 
   }
 
+  invalidRegisterPet() {
+    cy.fixture('../fixtures/env/prod/general_data.json').then((data) => {
+      this.CommonElements.registerBtn().click();
+      this.CommonElements.newPetBtn().click();
+      this.CommonElements.registerMessage().should('contain', 'New Pet registration');
+      this.CommonElements.petOwner().type(data.users.name[0]+Math.floor(Math.random() * 100));
+      this.CommonElements.petName().type('Niko Spencer');
+      this.CommonElements.petColor().select('brown');
+      this.CommonElements.petAge().type('2');
+      this.CommonElements.petDescription().type('...');
+      this.CommonElements.specieDog().click();
+      this.CommonElements.petVaccinationStatus().select('true');
+      this.CommonElements.petMembership().select('false');
+      this.CommonElements.service().check('bath');
+      this.CommonElements.service().check('walk');
+      this.CommonElements.service().check('spa');
+      this.CommonElements.register().click();
+    });
+  }
+  
   deletePet() {
     cy.visit('/my_pet_registrations');
     this.CommonElements.table().should('contain', 'Fido');
